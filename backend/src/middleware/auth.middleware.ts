b20b9d558b07c7c24c0dc070ea61b4747e39407c
Philip Yaw Boahene <boahenep22@gmail.com>
2025-03-19 14:28:15 -0700

    basics of backend done, still working

---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const checkLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
+  console.log("Session:", req.session);
+  if (req.session && req.session.isLoggedIn) {
+    next()
+    return
+  }
+  res.status(403).json({ message: "You are not allowed to access this resource!" })
+}
